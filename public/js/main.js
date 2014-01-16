3f0ce2a19a5375940ee5244d41ca8180a08d2f60
Brendan Nee <brendan@blinktag.com>
2014-01-16 11:17:14 -0800

    Handle MIL with no dtcs, plot point immediately without reverse geocoding

--- public/js/main.js
+++ public/js/main.js
@@ -43,16 +43,18 @@ ws.onmessage = function (msg) {
     } else if (data.type == 'region:changed') {
       description.push(data.region.status + ' ' + data.region.name + ' (' + data.region.tag + ')');
     } else if (data.type == 'mil:on' || data.type == 'mil:off') {
-      data.dtcs.forEach(function(dtc) { description.push(dtc.code + ': ' + dtc.description); });
+      if(data.dtcs) {
+        data.dtcs.forEach(function(dtc) { description.push(dtc.code + ': ' + dtc.description); });
+      }
     }
+
+    updateAlert(title, '');
+    addMarker(data.location.lat, data.location.lon, title, description.join('<br>'));
 
     geocoder.reverseQuery([data.location.lon, data.location.lat], function(e, response) {
       var location = buildLocation(response);
-      description.push('Location: ' + location);
-
-      addMarker(data.location.lat, data.location.lon, title, description.join('<br>'));
-      updateAlert(title, location);
       logMessage(title, date, location);
+      updateAlert(title, location);
     });
   }
 
