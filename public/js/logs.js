3bf2e1846974d37abb60e4e54c74a3358492f800
Brendan Nee <brendan@blinktag.com>
2014-02-28 16:01:02 -0800

    Added logs page

---
+++ public/js/logs.js
@@ -0,0 +1,40 @@
+$(function() {
+  $.getJSON('/logs/api/', function(data) {
+    data.map(formatItem);
+  });
+});
+
+function formatItem(item) {
+  var data = {}
+
+  if(item.trip) {
+    data.trip = item.trip;
+  }
+  if(item.speed_mph) {
+    data.speed_mph = item.speed_mph;
+  }
+  if(item.g_force) {
+    data.g_force = item.g_force;
+  }
+  if(item.region) {
+    data.region = item.region;
+  }
+  if(item.dtcs) {
+    data.dtcs = item.dtcs;
+  }
+  if(item.user_cleared) {
+    data.user_cleared = item.user_cleared;
+  }
+
+  var row = []
+
+  row.push(moment(item.created_at, time_zone).format('YYYY-MM-DD H:MM A'));
+  row.push(item.type);
+  row.push(item.vehicle.display_name);
+  row.push('<a href="https://www.google.com/maps/place/' + item.location.lat + ',' + item.location.lon + '">map</a>');
+  row.push(JSON.stringify(data));
+
+  $('<tr>')
+    .append(row.map(function(cell) { return '<td>' + cell + '</td>'; }))
+    .appendTo('#logs');
+}
