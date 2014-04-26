48dfe526fc6e95e560a119e4c6b8998c206c5547
Gabi Zuniga <gabiz@alignd.io>
2014-04-26 14:15:09 -0700

    fix sort?

--- database.js
+++ database.js
@@ -5,17 +5,12 @@ var webhook_logs = db.get('automaticator');
 webhook_logs.index({ '_id': -1 });
 
 exports.getLogs = function(user_id, cb) {
-  webhook_logs.find({user: {id: user_id} }, cb);
+  // webhook_logs.find({user: {id: user_id} }, cb);
+  webhook_logs.find({ user: {id: user_id} }, {sort:[['_id',-1]]}, cb);
 }
 
-exports.getReverseLogs = function(user_id, cb) {
-  // query._id={$gt:0}
-
-  // db.driver.webhook_logs.findItems(query,null,{limit:3,sort:[['_id',1]]},function(err,list) {
-  //   cb(list);
-  // })
-  webhook_logs.find({}, {limit:3,sort:[['_id',-1]]}, cb);
-  // webhook_logs.find({user: {id: user_id} }, {sort:['_id',-1]}, cb);
+exports.getReverseAllLogs = function(cb) {
+  webhook_logs.find({}, {limit:10,sort:[['_id',-1]]}, cb);
 }
 
 exports.saveLog = function(body) {
--- routes/api.js
+++ routes/api.js
@@ -11,21 +11,24 @@ exports.logs = function(req, res) {
 }
 
 exports.reverseLogs = function(req, res) {
-  if(req.session.user_id) {
-    db.getReverseLogs(req.session.user_id, function(e, docs) {
-      res.json(docs);
-    });
-  } else {
-    res.json({error: 'Not logged in'});
-  }
+  db.getReverseLogs(function(e, docs) {
+    res.json(docs);
+  });
+  // if(req.session.user_id) {
+  // } else {
+  //   res.json({error: 'Not logged in'});
+  // }
 }
 
 exports.lastLog = function(req, res) {
-  if(req.session.user_id) {
-    db.getReverseLogs(req.session.user_id, function(e, docs) {
-      res.json(docs[0]);
-    });
-  } else {
-    res.json({error: 'Not logged in'});
-  }
+  db.getReverseLogs(req.session.user_id, function(e, docs) {
+    res.json(docs[0]);
+  });
+  // if(req.session.user_id) {
+  //   db.getReverseLogs(req.session.user_id, function(e, docs) {
+  //     res.json(docs[0]);
+  //   });
+  // } else {
+  //   res.json({error: 'Not logged in'});
+  // }
 }
