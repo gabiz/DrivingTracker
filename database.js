2a8d36b6db0cb76cd25d128efac030d161c7918e
Gabi Zuniga <gabiz@alignd.io>
2014-04-26 11:16:17 -0700

    changed db

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 var monk = monk = require('monk');
-var db = monk(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/automaticator');
+var db = monk(process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/automaticator');
 var webhook_logs = db.get('automaticator');
 
 exports.getLogs = function(user_id, cb) {
