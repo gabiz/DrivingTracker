395b6e7c05a4ccddd7c6dd6e21de4072f1a2db36
Gabi Zuniga <gabiz@alignd.io>
2014-04-26 13:52:34 -0700

    fix sort?

--- database.js
+++ database.js
@@ -2,13 +2,15 @@ var monk = monk = require('monk');
 var db = monk(process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/automaticator');
 var webhook_logs = db.get('automaticator');
 
+webhook_logs.index({ '_id': -1 });
+
 exports.getLogs = function(user_id, cb) {
   webhook_logs.find({user: {id: user_id} }, cb);
 }
 
 exports.getReverseLogs = function(user_id, cb) {
-
-  webhook_logs.find({user: {id: user_id} }, {sort:['_id',-1]}, cb);
+  webhook_logs.find({}, cb);
+  // webhook_logs.find({user: {id: user_id} }, {sort:['_id',-1]}, cb);
 }
 
 exports.saveLog = function(body) {
